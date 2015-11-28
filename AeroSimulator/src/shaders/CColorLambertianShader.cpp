7e1a714332d88932b0e37ed849bdef59d08a7173
Nikolay <prodk@rambler.ru>
2015-11-28 23:11:54 +0200

    Fixed more bugs in diffuse lighting.

--- AeroSimulator/src/shaders/CColorLambertianShader.cpp
+++ AeroSimulator/src/shaders/CColorLambertianShader.cpp
@@ -19,11 +19,11 @@ CColorLambertianShader::CColorLambertianShader()
       "uniform mat4 MVP;\n"
       "uniform mat4 uM;\n"
       //"uniform mat4 uV;\n"         // View matrix
-      "varying vec4 vEyeNormal;\n"
-      "varying vec4 vPos;\n"
+      "varying vec3 vEyeNormal;\n"
+      "varying vec3 vPos;\n"
       "void main(){\n"
-      "    vEyeNormal = uM * vec4(aNormal, 1.0);\n"
-      "    vPos = uM * vec4(aPosition, 1.0);\n"
+      "    vEyeNormal = (uM * vec4(aNormal, 0.0)).xyz;\n"
+      "    vPos = (uM * vec4(aPosition, 1.0)).xyz;\n"
       "    gl_Position = MVP * vec4(aPosition, 1.0);\n"
       "}\n";
 
@@ -32,21 +32,21 @@ CColorLambertianShader::CColorLambertianShader()
       "uniform vec3 uAmbient;\n"
       "uniform vec3 uDiffuse;\n"
       "uniform vec3 uSunPos;\n"
-      "varying vec4 vEyeNormal;\n"
-      "varying vec4 vPos;\n"
+      "varying vec3 vEyeNormal;\n"
+      "varying vec3 vPos;\n"
 
       "const vec4 lightColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
       "const vec4 eyePos = vec4(0.0, 0.0, 13.0, 1.0);\n"
       "void main(){\n"
-      "    vec4 N = normalize(vEyeNormal);\n"
-      "    vec4 L = vec4(uSunPos, 1.0);\n"
-      "    float cosD = clamp(dot(N, L), 0.0, 1.0);\n"
-      "    vec4 cameraDir = normalize(eyePos - vPos);\n"
+      "    vec3 N = normalize(vEyeNormal);\n"
+      "    vec3 L = normalize(uSunPos);\n"
+      "    float cosD = clamp(dot(N, L), 0, 1);\n"
+      //"    vec4 cameraDir = normalize(eyePos - vPos);\n"
 
       //"    vec4 E = normalize(vEyeDirection);\n"
-      "    vec4 R = reflect(-L, N);\n"
-      "    float cosS = clamp(dot(cameraDir, R), 0.0, 1.0);\n"
-      "    gl_FragColor = uColor * vec4(uAmbient + uDiffuse * cosD + pow(cosS, 20), 1);\n"
+      //"    vec4 R = reflect(-L, N);\n"
+      //"    float cosS = clamp(dot(cameraDir, R), 0.0, 1.0);\n"
+      "    gl_FragColor = lightColor * vec4(uDiffuse * cosD, 1);//vec4(uAmbient + uDiffuse * cosD + pow(cosS, 20), 1);\n"
       "}\n";
 
    CLog::getInstance().log("* CColorLambertianShader created");
@@ -107,10 +107,10 @@ void CColorLambertianShader::setup(CRenderable & renderable)
       const glm::vec3 ambientColor(0.2f, 0.1f, 0.0f);
       glUniform3fv(mAmbientUniformId, 1, &(ambientColor.r));
 
-      const glm::vec3 diffuseColor(0.7f, 0.4f, 0.0f);
+      const glm::vec3 diffuseColor(0.0f, 0.8f, 0.0f);
       glUniform3fv(mDiffuseUniformId, 1, &(diffuseColor.r));
 
-      const glm::vec3 sunPosition(-1.0f, 0.0f, 0.0f);
+      const glm::vec3 sunPosition(-1.0f, 1.0f, 0.0f);
       glUniform3fv(mSunPosUniformId, 1, &(sunPosition.x));
 
       ///@todo: remove
