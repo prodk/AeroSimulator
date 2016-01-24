9dc308cc78850126d1d78ebb6af6fd265e89a74b
Nikolay <prodk@rambler.ru>
2016-01-24 17:41:59 +0200

    Refactoring: changed adding shader params to CRenderable.

--- AeroSimulator/CRenderable.cpp
+++ AeroSimulator/CRenderable.cpp
@@ -1,35 +1,34 @@
-
 #include "CRenderable.h"
-#include "../src/shaders/CShader.h"
 #include "CGeometry.h"
-#include "CCommonMath.h"
+#include "../src/shaders/CShader.h"
+#include "CLog.h"
+//#include "CCommonMath.h"
 
-#include "glm/mat3x3.hpp"
-#include "glm/gtc/matrix_transform.hpp"
+//#include "glm/gtc/matrix_transform.hpp"
 
 using namespace AeroSimulatorEngine;
 
 CRenderable::CRenderable()
    : mGeometry()
    , mShader()
-   , mModelMatrix()
-   , mMvpMatrix()
+   /*, mModelMatrix()
+   , mMvpMatrix()*/
    , mTexture()
    , mVboId(0)
    , mIboId(0)
-   , mRightVector()
-   , mUpVector()
-   , mBillboardWidth(1.0f)
-   , mBillboardHeight(1.0f)
+   //, mRightVector()
+   //, mUpVector()
+   //, mBillboardWidth(1.0f)
+   //, mBillboardHeight(1.0f)
    , mDrawWithLines(false)
-   , mColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f))
-   , mLineWidth(2.0f)
-   , mHealthValue(0.0f)
-   , mViewMatrix()
-   , mEyePos()
-   , mCurrentFrame()
-   , mFrameSize()
-   , mNumOfFrames()
+   //, mColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f))
+   //, mLineWidth(2.0f)
+   //, mHealthValue(0.0f)
+   //, mViewMatrix()
+   //, mEyePos()
+   //, mCurrentFrame()
+   //, mFrameSize()
+   //, mNumOfFrames()
    , mIsVisible(true)
    , mNormalMapTexture()
    , mAnimationTexture()
@@ -54,6 +53,179 @@ void CRenderable::setEnvironment()
 {
 }
 
+void CRenderable::setFlag(const int id, const bool value)
+{
+   const std::pair<int, bool> element(id, value);
+   mFlags.insert(element);
+}
+
+void CRenderable::set1DParam(const int id, const float value)
+{
+   const std::pair<int, float> element(id, value);
+   m1DParams.insert(element);
+}
+
+void CRenderable::setVector2Param(const int id, const glm::vec2& value)
+{
+   const std::pair<int, glm::vec2> element(id, value);
+   mVector2Params.insert(element);
+}
+
+void CRenderable::setVector3Param(const int id, const glm::vec3& value)
+{
+   const std::pair<int, glm::vec3> element(id, value);
+   mVector3Params.insert(element);
+}
+
+void CRenderable::setVector4Param(const int id, const glm::vec4 & value)
+{
+   const std::pair<int, glm::vec4> element(id, value);
+   mVector4Params.insert(element);
+}
+
+void CRenderable::setMatrix3Param(const int id, const glm::mat3 & value)
+{
+   const std::pair<int, glm::mat3> element(id, value);
+   mMatrix3Params.insert(element);
+}
+
+void CRenderable::setMatrix4Param(const int id, const glm::mat4& value)
+{
+   const std::pair<int, glm::mat4> element(id, value);
+   mMatrix4Params.insert(element);
+}
+
+bool CRenderable::getFlag(const int id) const
+{
+   ///@todo: try when compiles findValueInMap<bool>(mFlags, id)
+   auto result = false;
+
+   auto iter = mFlags.find(id);
+   if (mFlags.end() != iter)
+   {
+      result = iter->second;
+   }
+   else
+   {
+      CLog::getInstance().log("Failed to find a flag: ", id);
+   }
+
+   return result;
+}
+
+float CRenderable::get1DParam(const int id) const
+{
+   ///@todo: try when compiles return findValueInMap<float>(m1DParams, id);
+   auto result = 0.0f;
+
+   auto iter = m1DParams.find(id);
+   if (m1DParams.end() != iter)
+   {
+      result = iter->second;
+   }
+   else
+   {
+      CLog::getInstance().log("Failed to find 1D param: ", id);
+   }
+
+   return result;
+}
+
+glm::vec2 CRenderable::getVector2Param(const int id) const
+{
+   ///@todo: when compiles try:
+   //return findValueInMap<glm::vec2>(m2DParams, id);
+   auto result = glm::vec2();
+
+   auto iter = mVector2Params.find(id);
+   if (mVector2Params.end() != iter)
+   {
+      result = iter->second;
+   }
+   else
+   {
+      CLog::getInstance().log("Failed to find vector 2D: ", id);
+   }
+
+   return result;
+}
+
+glm::vec3 CRenderable::getVector3Param(const int id) const
+{
+   ///@todo: when compiles try:
+   //return findValueInMap<glm::vec3>(m3DParams, id);
+   auto result = glm::vec3();
+
+   auto iter = mVector3Params.find(id);
+   if (mVector3Params.end() != iter)
+   {
+      result = iter->second;
+   }
+   else
+   {
+      CLog::getInstance().log("Failed to find vector 3D: ", id);
+   }
+
+   return result;
+}
+
+glm::vec4 CRenderable::getVector4Param(const int id) const
+{
+   ///@todo: when compiles try:
+   //return findValueInMap<glm::vec4>(mVector4Params, id);
+   auto result = glm::vec4();
+
+   auto iter = mVector4Params.find(id);
+   if (mVector4Params.end() != iter)
+   {
+      result = iter->second;
+   }
+   else
+   {
+      CLog::getInstance().log("Failed to find vector 4D: ", id);
+   }
+
+   return result;
+}
+
+glm::mat3 CRenderable::getMatrix3Param(const int id) const
+{
+   ///@todo: when compiles try:
+   //return findValueInMap<glm::mat4>(m4DParams, id);
+   auto result = glm::mat3();
+
+   auto iter = mMatrix3Params.find(id);
+   if (mMatrix3Params.end() != iter)
+   {
+      result = iter->second;
+   }
+   else
+   {
+      CLog::getInstance().log("Failed to find matrix 3D: ", id);
+   }
+
+   return result;
+}
+
+glm::mat4 CRenderable::getMatrix4Param(const int id) const
+{
+   ///@todo: when compiles try:
+   //return findValueInMap<glm::mat4>(m4DParams, id);
+   auto result = glm::mat4();
+
+   auto iter = mMatrix4Params.find(id);
+   if (mMatrix4Params.end() != iter)
+   {
+      result = iter->second;
+   }
+   else
+   {
+      CLog::getInstance().log("Failed to find matrix 4D: ", id);
+   }
+
+   return result;
+}
+
 bool CRenderable::loadTexture(const char * filePath)
 {
    return false;
@@ -69,15 +241,15 @@ bool CRenderable::loadAnimationTexture(const char * filePath)
    return false;
 }
 
-glm::mat3 CRenderable::getNormalMatrix() const
-{
-   glm::mat3x3 result;
-   CCommonMath::copyColumn(0, result, mModelMatrix);
-   CCommonMath::copyColumn(1, result, mModelMatrix);
-   CCommonMath::copyColumn(2, result, mModelMatrix);
-
-   result = glm::inverse(result);
-   result = glm::transpose(result);
-
-   return result;
-}
+//glm::mat3 CRenderable::getNormalMatrix() const
+//{
+//   glm::mat3x3 result;
+//   CCommonMath::copyColumn(0, result, mModelMatrix);
+//   CCommonMath::copyColumn(1, result, mModelMatrix);
+//   CCommonMath::copyColumn(2, result, mModelMatrix);
+//
+//   result = glm::inverse(result);
+//   result = glm::transpose(result);
+//
+//   return result;
+//}
--- AeroSimulator/CRenderable.h
+++ AeroSimulator/CRenderable.h
@@ -3,11 +3,27 @@
 #ifndef AERO_SIMULATOR_CRENDERABLE_H
 #define AERO_SIMULATOR_CRENDERABLE_H
 
-#include "../AeroSimulator/include/glew.h"
-#include "../AeroSimulator/include/glm/mat4x4.hpp"
-#include "../AeroSimulator/include/glm/vec3.hpp"
-#include "../AeroSimulator/include/glm/vec2.hpp"
+//#include "../AeroSimulator/include/glew.h"
 #include <memory>
+#include <map>
+
+#include "../AeroSimulator/include/glm/vec2.hpp"
+#include "../AeroSimulator/include/glm/vec3.hpp"
+#include "../AeroSimulator/include/glm/mat3x3.hpp"
+#include "../AeroSimulator/include/glm/mat4x4.hpp"
+
+namespace
+{
+   // Global constants for setting rendering flags and shader uniforms.
+   enum eShaderFlags { REPEAT_TEXTURE, DRAW_LINES, IS_VISIBLE, IS_TRANSPARENT };
+   enum eShader1DParams { VBO0_ID, IBO0_ID, BILLBOARD_WIDTH, BILLBOARD_HEIGHT, LINE_WIDTH, HEALTH };
+   enum eShaderVector2Params { CURRENT_FRAME, FRAME_SIZE, NUM_OF_FRAMES };
+   enum eShaderVector3Params { EYE_POSITION, RIGHT_VECTOR, UP_VECTOR };
+   enum eShaderVector4Params { COLOR };
+   enum eShaderMatrix3Params { NORMAL_MATRIX };
+   enum eShaderMatrix4Params { MODEL_MATRIX, MVP_MATRIX, VIEW_MATRIX };
+}
+
 
 namespace AeroSimulatorEngine
 {
@@ -28,7 +44,26 @@ namespace AeroSimulatorEngine
       virtual void resetEnvironment();
       bool canBeRendered() const { return (0 != mGeometry) && (0 != mShader); }
 
+      // Setters for shader params
+      void setFlag(const int id, const bool value);
+      void set1DParam(const int id, const float value);
+      void setVector2Param(const int id, const glm::vec2& value);
+      void setVector3Param(const int id, const glm::vec3& value);
+      void setVector4Param(const int id, const glm::vec4& value);
+      void setMatrix3Param(const int id, const glm::mat3& value);
+      void setMatrix4Param(const int id, const glm::mat4& value);
+
+      // Getters for shader params
+      bool getFlag(const int id) const;
+      float get1DParam(const int id) const;
+      glm::vec2 getVector2Param(const int id) const;
+      glm::vec3 getVector3Param(const int id) const;
+      glm::vec4 getVector4Param(const int id) const;
+      glm::mat3 getMatrix3Param(const int id) const;
+      glm::mat4 getMatrix4Param(const int id) const;
+
       /// Textures
+      ///@todo: probably remove load texture methods, it should be invoked via textures
       virtual bool loadTexture(const char* filePath);
       virtual bool loadNormalMapTexture(const char* filePath);
       virtual bool loadAnimationTexture(const char* filePath);
@@ -36,8 +71,8 @@ namespace AeroSimulatorEngine
       CTexture* getNormalMapTexture() const { return mNormalMapTexture.get(); }
       CTexture* getAnimationTexture() const { return mAnimationTexture.get(); }
 
-      void setRepeatTexture(const bool repeat) { mRepeatTexture = repeat; }
-      bool isRepeatTexture() const { return mRepeatTexture; }
+      //void setRepeatTexture(const bool repeat) { mRepeatTexture = repeat; }
+      //bool isRepeatTexture() const { return mRepeatTexture; }
 
       /// Geometry
       void setGeometry(std::shared_ptr<CGeometry>& pGeometry) { mGeometry = pGeometry; }
@@ -47,117 +82,169 @@ namespace AeroSimulatorEngine
       void setShader(CShader* pShader) { mShader.reset(pShader); }
       CShader* getShader() const { return mShader.get(); }
 
-      GLuint getVboId() const { return mVboId; }
-      GLuint getIboId() const { return mIboId; }
+      //GLuint getVboId() const { return mVboId; }
+      //GLuint getIboId() const { return mIboId; }
 
       void setTexture(std::shared_ptr<CTexture>& pTexture) { mTexture = pTexture; }
 
       /// Matrices
-      void setModelMatrix(const glm::mat4& m) { mModelMatrix = m; }
-      glm::mat4 getModelMatrix() const { return mModelMatrix; }
+      //void setModelMatrix(const glm::mat4& m) { mModelMatrix = m; }
+      //glm::mat4 getModelMatrix() const { return mModelMatrix; }
 
-      void setMvpMatrix(const glm::mat4& m) { mMvpMatrix = m; }
-      glm::mat4 getMvpMatrix() const { return mMvpMatrix; }
+      //void setMvpMatrix(const glm::mat4& m) { mMvpMatrix = m; }
+      //glm::mat4 getMvpMatrix() const { return mMvpMatrix; }
 
-      void setViewMatrix(const glm::mat4& m) { mViewMatrix = m; }
-      glm::mat4 getViewMatrix() const { return mViewMatrix; }
+      //void setViewMatrix(const glm::mat4& m) { mViewMatrix = m; }
+      //glm::mat4 getViewMatrix() const { return mViewMatrix; }
 
-      glm::mat3 getNormalMatrix() const;
+      //glm::mat3 getNormalMatrix() const;
 
-      void setEyePos(const glm::vec3& eyePos) { mEyePos = eyePos; }
-      glm::vec3 getEyePos() const { return mEyePos; }
+      //void setEyePos(const glm::vec3& eyePos) { mEyePos = eyePos; }
+      //glm::vec3 getEyePos() const { return mEyePos; }
 
       /// Billboards
-      void setRightVector(const glm::vec3 & right) { mRightVector = right; }
-      glm::vec3 getRightVector() const { return mRightVector; }
-      void setUpVector(const glm::vec3 & up) { mUpVector = up; }
-      glm::vec3 getUpVector() const { return mUpVector; }
+      //void setRightVector(const glm::vec3 & right) { mRightVector = right; }
+      //glm::vec3 getRightVector() const { return mRightVector; }
+      //void setUpVector(const glm::vec3 & up) { mUpVector = up; }
+      //glm::vec3 getUpVector() const { return mUpVector; }
 
-      void setBillboardWidth(const float width) { mBillboardWidth = width; }
-      float getBillboardWidth() const { return mBillboardWidth; }
-      void setBillboardHeight(const float height) { mBillboardHeight = height; }
-      float getBillboardHeight() const { return mBillboardHeight; }
+      //void setBillboardWidth(const float width) { mBillboardWidth = width; }
+      //float getBillboardWidth() const { return mBillboardWidth; }
+      //void setBillboardHeight(const float height) { mBillboardHeight = height; }
+      //float getBillboardHeight() const { return mBillboardHeight; }
 
-      void setDrawWithLines(bool flag) { mDrawWithLines = flag; }
-      bool getDrawWithLines() const { return mDrawWithLines; }
+      //void setDrawWithLines(bool flag) { mDrawWithLines = flag; }
+      //bool getDrawWithLines() const { return mDrawWithLines; }
 
       // Uniform color renderables-specific
-      void setColor(const glm::vec4& color) { mColor = color; }
-      glm::vec4 getColor() const { return mColor; }
+      //void setColor(const glm::vec4& color) { mColor = color; }
+      //glm::vec4 getColor() const { return mColor; }
 
       // Line-specific
-      void setLineWidth(const float width) { mLineWidth = width; }
-      float getLineWidth() const { return mLineWidth; }
+      //void setLineWidth(const float width) { mLineWidth = width; }
+      //float getLineWidth() const { return mLineWidth; }
 
-      void setHealthValue(float shift) { mHealthValue = shift; }
-      float getHealthValue() const { return mHealthValue; }
+      //void setHealthValue(float shift) { mHealthValue = shift; }
+      //float getHealthValue() const { return mHealthValue; }
 
       // Animation-specific
-      void setCurrentFrame(const glm::vec2& frame) { mCurrentFrame = frame; }
-      glm::vec2 getCurrentFrame() const { return mCurrentFrame; }
+      //void setCurrentFrame(const glm::vec2& frame) { mCurrentFrame = frame; }
+      //glm::vec2 getCurrentFrame() const { return mCurrentFrame; }
 
-      void setFrameSize(const glm::vec2& size)
-      { 
-         mFrameSize = size; 
+      /*void setFrameSize(const glm::vec2& size)
+      {
+         mFrameSize = size;
          if (mFrameSize.length())
          {
             mNumOfFrames.x = 1.f / mFrameSize.x;
             mNumOfFrames.y = 1.f / mFrameSize.y;
          }
-      }
-      glm::vec2 getFrameSize() const { return mFrameSize; }
+      }*/
+      //glm::vec2 getFrameSize() const { return mFrameSize; }
 
-      bool isVisible() const { return mIsVisible; }
+      //bool isVisible() const { return mIsVisible; }
 
-      void setTransparent(const bool flag) { mIsTransparent = flag; }
-      bool isTransparent() const { return mIsTransparent; }
+      //void setTransparent(const bool flag) { mIsTransparent = flag; }
+      //bool isTransparent() const { return mIsTransparent; }
 
-      virtual void setVisible(const bool visible) { mIsVisible = visible; }
+      //virtual void setVisible(const bool visible) { mIsVisible = visible; }
 
       void setTextureUnit(const GLint unit) { mTextureUnit = unit; }
       GLint getTextureUnit() const { return mTextureUnit; }
 
-   protected:
+   private:
+      template <typename T>
+      T findValueInMap(std::map<int, T>& sourceMap, const int key);
+
+   //protected:
+   private:
+      ///@todo: rearrange this params as well as in the initialization list
       std::shared_ptr<CGeometry> mGeometry;
       std::shared_ptr<CShader> mShader;
-      glm::mat4 mModelMatrix;
-      glm::mat4 mMvpMatrix;
+      //glm::mat4 mModelMatrix;
+      //glm::mat4 mMvpMatrix;
+      ///@todo: add an enum with texture IDs {USUAL, NORAML_MAP, ANIMATION}, then add textures to a vector or a map, see shaders in CGame
       std::shared_ptr<CTexture> mTexture;
-      GLuint mVboId;
-      GLuint mIboId;
+      //GLuint mVboId;
+      //GLuint mIboId;
 
       ///@todo: probably create a separate renderable for a billboard
       /// These are used only fro billboards
-      glm::vec3 mRightVector;
-      glm::vec3 mUpVector;
-      float mBillboardWidth;
-      float mBillboardHeight;
+      //glm::vec3 mRightVector;
+      //glm::vec3 mUpVector;
+      //float mBillboardWidth;
+      //float mBillboardHeight;
 
-      bool mDrawWithLines;
-      glm::vec4 mColor; // Only for color shader
-      float mLineWidth; // Only for debug mode
+      //bool mDrawWithLines;
+      //glm::vec4 mColor; // Only for color shader
+      //float mLineWidth; // Only for debug mode
 
       ///@todo: change the architecture such that it is not required to add all
       ///@todo these variables to CRenderer. They should be added to those objects where they are needed!
       ///@todo: or create several types of renderables for each type of objects
-      float mHealthValue; // Shift of the healthbar foreground relative to the background
-      glm::mat4 mViewMatrix;
-      glm::vec3 mEyePos;
+      //float mHealthValue; // Shift of the healthbar foreground relative to the background
+      //glm::mat4 mViewMatrix;
+      //glm::vec3 mEyePos;
 
       // Animation-related
-      glm::vec2 mCurrentFrame;
-      glm::vec2 mFrameSize;
-      glm::vec2 mNumOfFrames;
+      //glm::vec2 mCurrentFrame;
+      //glm::vec2 mFrameSize;
+      //glm::vec2 mNumOfFrames;
 
-      bool mIsVisible;
+      //bool mIsVisible;
 
       std::shared_ptr<CTexture> mNormalMapTexture;
       std::shared_ptr<CTexture> mAnimationTexture;
 
-      bool mRepeatTexture;
-      bool mIsTransparent;
+      //bool mRepeatTexture;
+      //bool mIsTransparent;
       GLint mTextureUnit;
+
+      // The maps contain flags and parameters used mainly in shaders as uniform values.
+      // A shader gives the name of the parameter and gets its value
+      // In this way we avoid hardcodig unnecessary values in every instance of this class.
+
+      ///@todo: add to initializing list these maps
+      // Flags, e.g. drawWithLines, isVisible, repeat texture, isTransparent
+      std::map<int, bool> mFlags;
+
+      // 1D parameters ints and floats, ints are saved as floats
+      std::map<int, float> m1DParams;
+
+      // 2D vectors
+      std::map<int, glm::vec2> mVector2Params;
+
+      // 3D vectors
+      std::map<int, glm::vec3> mVector3Params;
+
+      // 4D vectors
+      std::map<int, glm::vec4> mVector4Params;
+
+      // 3x3 matrices
+      std::map<int, glm::mat3> mMatrix3Params;
+
+      // 4x4 matrices
+      std::map<int, glm::mat4> mMatrix4Params;
    };
+
+   template <typename T>
+   T CRenderable::findValueInMap(std::map<int, T>& sourceMap, const int key)
+   {
+      T result = T();
+
+      auto iter = sourceMap.find(key);
+      if (sourceMap.end() != iter)
+      {
+         result = iter->second;
+      }
+      ///@todo: uncomment when compiles
+      /*else
+      {
+         CLog::getInstance().log("Failed to find: ", key);
+      }*/
+
+      return result;
+   }
 } // namespace AeroSimulatorEngine
 
 #endif // AERO_SIMULATOR_CRENDERABLE_H
